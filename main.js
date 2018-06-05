e6fcfb220fc9464992ea4713538b1597c83cc8f6
BeBarefoot <35373216+BeBarefoot@users.noreply.github.com>
2018-06-05 22:01:50 +0300

    COMMIT

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', wrap => {
+
     let wrapper = document.getElementById('wrapper');
     let topLayer = wrapper.querySelector('.top');
     let handle = wrapper.querySelector('.handle');
     let skew = 0;
     let delta = 0;
+
     if (wrapper.className.indexOf('skewed') != -1) {
-        skew = 1000;
-    }
+    skew = 1000}
 
     wrapper.addEventListener('mousemove', e => {
         delta = (e.clientX - window.innerWidth / 2) * 0.5;
         handle.style.left = e.clientX + delta + 'px';
-
         topLayer.style.width = e.clientX + skew + delta + 'px';
-    });
+    });   
 });
 
-
-
-
-playWill => document.getElementById("playWill").play();
-
-play5ths => document.getElementById("play5ths").play();
+    document.getElementById("click5").addEventListener('click', play5ths => document.getElementById("play5ths").play());
+    document.getElementById("clickW").addEventListener('click', playWill => document.getElementById("playWill").play());
